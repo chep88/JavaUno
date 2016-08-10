e23f4eb15eaeab70097a840d3fac4b333dab57d7
Jean Pierre <jp.perezvaras@gmail.com>
2016-08-10 19:53:26 -0300

    Commit con la base del proyecto en netbeans

---
+++ JavaFundamentos/src/javafundamentos/JavaFundamentos.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafundamentos;
+
+/**
+ *
+ * @author trabajo
+ */
+public class JavaFundamentos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
