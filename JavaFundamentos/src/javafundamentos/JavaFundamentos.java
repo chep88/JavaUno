d6081122694cc33d6de29cd28579a119070724e9
Jean Pierre <jp.perezvaras@gmail.com>
2016-08-10 19:54:59 -0300

    Hola mundo en java con netbeans

--- JavaFundamentos/src/javafundamentos/JavaFundamentos.java
+++ JavaFundamentos/src/javafundamentos/JavaFundamentos.java
@@ -7,7 +7,7 @@ package javafundamentos;
 
 /**
  *
- * @author trabajo
+ * @author Jean Pierre 
  */
 public class JavaFundamentos {
 
@@ -15,7 +15,7 @@ public class JavaFundamentos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola mundo ");
     }
     
 }
